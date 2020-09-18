9e322db111a38b4e70ea7ad527f8cf37e4dc1c06
Bora Baykar <borabaykar@gmail.com>
2020-09-18 21:46:39 +0300

    Make some changes

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,20 +2,18 @@ from random import randint
 
 rand_num = randint(0, 20)
 
-# debug
-print(rand_num)
 while True:
-    guess = input("Enter a number between 0-20 = ")
+    guess = int(input("Enter a number between 0-20 = "))
 
-    if(int(guess) == rand_num):
+    if(guess == rand_num):
         print("Congrats you have found the number")
         break
-    
-    print("You have guessed it wrong, ")
 
-    if(int(guess) > rand_num):
+    elif(guess > rand_num):
         print("You must go lower")
 
-    if(int(guess) < rand_num):
+    elif(guess < rand_num):
         print("You must go higher")
-
+    
+    else:
+        print("Wrong Answer")
